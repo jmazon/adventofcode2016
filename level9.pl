c11e79d050340032eb5c3285cccaf400a0629290
Jean-Baptiste Mazon <jb@yavin>
2016-12-12 09:31:58 +0100

    Day 9

--- level9.pl
+++ level9.pl
@@ -2,9 +2,27 @@ use 5.020;
 use warnings;
 
 $_ = <>;
+chomp;
 
-while ( s/(.*?)\((\d+)x(\d+)\)// ) {
-  print $1, substr($', 0, $2) x $3;
-  s/.{$2}//;
+sub expand {
+  my $expr = shift;
+  my $length = 0;
+  while ( $expr =~ s/^(.*?)\((\d+)x(\d+)\)// ) {
+    $length += length $1;
+    my $substring = substr($expr, 0, $2, '');
+    my $repeat = $3;
+    $length += $repeat * expand( $substring );
+  }
+  return $length + length $expr;
 }
-print;
+
+if (0) {
+  while ( s/(.*?)\((\d+)x(\d+)\)// ) {
+    print $1, substr($', 0, $2) x $3;
+    s/.{$2}//;
+  }
+  say;
+}
+else {
+  say expand($_);
+}
